f8cebb1cdb97c45e0441c927140a3d2142966598
1324151534 <s1324151534@outlook.com>
2023-09-17 12:25:03 +0800

    Update 1.1
    Use C++ to Rebuild Explosive Barrel Class

---
+++ Source/GameDevelop/Private/XExplosiveBarrel.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "XExplosiveBarrel.h"
+#include "PhysicsEngine/RadialForceComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "UObject/ConstructorHelpers.h"
+
+// Sets default values
+AXExplosiveBarrel::AXExplosiveBarrel()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	StaticMesh_1 = CreateDefaultSubobject<UStaticMeshComponent>("StaticMesh_1");
+	StaticMesh_1->SetNotifyRigidBodyCollision(true);
+	StaticMesh_1->SetSimulatePhysics(true);
+	StaticMesh_1->SetCollisionProfileName("PhysicsActor");
+	StaticMesh_1->OnComponentHit.AddDynamic(this, &AXExplosiveBarrel::AXOnHit);
+	RootComponent = StaticMesh_1;
+
+	RadialForceComp = CreateDefaultSubobject<URadialForceComponent>("RadialForceComp");
+	RadialForceComp->SetupAttachment(StaticMesh_1);
+	RadialForceComp->ImpulseStrength = 2000.0;
+	RadialForceComp->bImpulseVelChange = true;
+	RadialForceComp->Radius = 600.0;
+}
+
+// Called when the game starts or when spawned
+void AXExplosiveBarrel::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AXExplosiveBarrel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AXExplosiveBarrel::AXOnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	RadialForceComp->FireImpulse();
+}
+
---
+++ Source/GameDevelop/Public/XExplosiveBarrel.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "XExplosiveBarrel.generated.h"
+
+class URadialForceComponent;
+class UStaticMeshComponent;
+
+UCLASS()
+class GAMEDEVELOP_API AXExplosiveBarrel : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AXExplosiveBarrel();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* StaticMesh_1;
+
+	UPROPERTY(VisibleAnywhere)
+	URadialForceComponent* RadialForceComp;
+
+	UFUNCTION()
+	void AXOnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
